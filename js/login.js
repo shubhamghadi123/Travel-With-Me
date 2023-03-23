7f6431dbf6bac0e23cf799bcc9e062f9d0509563
Shubham <shubhamghadi60@gmail.com>
2023-03-23 18:28:16 +0530

    added javascript in register page

--- js/login.js
+++ js/login.js
@@ -1,8 +1,20 @@
-const loginForm = document.getElementById('form');
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
 const errorMsg = document.getElementById('error-msg');
-const submitBtn = document.getElementById('login-btn');
+const loginBtn = document.getElementById('login-btn');
+const registerBtn = document.getElementById('register-btn');
 
-submitBtn.addEventListener('click', (e) => {
+loginBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if(username === "" || password === ""){
+        errorMsg.classList.remove("visually-hidden");
+    }
+})
+
+registerBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
