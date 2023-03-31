2555d9f9c4f527ba2d48d85426f90242be10bebe
Shubham <shubhamghadi60@gmail.com>
2023-03-31 17:00:04 +0530

    corrected javascript for register page

--- js/register.js
+++ js/register.js
@@ -1,44 +1,42 @@
-const registerForm = document.getElementById('register-form');
-const registerBtn = document.getElementById('register-btn');
-const regErrorMsg = document.getElementById('reg-error-msg');
-const confirmPassErrorMsg = document.getElementById('password-error-msg');
-const passErrorMsg = document.getElementById('password2-error-msg');
-const passContains = document.getElementById('passwordcontains-error-msg');
-const emailErrorMsg = document.getElementById('email-error-msg');
-
-
-registerBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const username = registerForm.username.value;
-    const password = registerForm.password.value;
-    const confirmPassword = registerForm.confirmPassword.value;
-    const email = registerForm.email.value;
+function validate(){
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    const regErrorMsg = document.getElementById('reg-error-msg');
+    const emailErrorMsg = document.getElementById('email-error-msg');
+    const confirmPassErrorMsg = document.getElementById('password-error-msg');
+    const passContains = document.getElementById('passwordcontains-error-msg');
+    const passErrorMsg = document.getElementById('password2-error-msg');
     var regEmail = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
-
+    var upper = /[A-Z]/
+    var lower = /[a-z]/
+    var number = /[0-9]/
+    var specialCharacter = /[!\@\#\$\%\^\&\*\_\=\+\-\?\>\<\.\,]/
+    
     regErrorMsg.classList.add("visually-hidden");
+    emailErrorMsg.classList.add("visually-hidden");
     confirmPassErrorMsg.classList.add("visually-hidden");
     passErrorMsg.classList.add("visually-hidden");
     passContains.classList.add("visually-hidden");
-    emailErrorMsg.classList.add("visually-hidden");
 
-    if(username === "" && email === "" && password === "" && confirmPassword === ""){
+    if(username === "" || email === "" || password === "" || confirmPassword === ""){
         regErrorMsg.classList.remove("visually-hidden");
     }
     else{
-        if(password != confirmPassword){
-            confirmPassErrorMsg.classList.remove("visually-hidden");
-        }
-    
         if(password.length > 1 && password.length < 8){
             passErrorMsg.classList.remove("visually-hidden");
         }
-    
-        if(password.value != /^([a-zA-Z0-9\!@#$%^&*_+?])$/){
+        else if(password != confirmPassword){
+            confirmPassErrorMsg.classList.remove("visually-hidden");
+        }
+        else if(!password.match(upper) || !password.match(lower)
+        || !password.match(number) || !password.match(specialCharacter)){
             passContains.classList.remove("visually-hidden");
         }
-    
-        if(email.value != regEmail){
+
+        if(!email.match(regEmail)){
             emailErrorMsg.classList.remove("visually-hidden");
         }
     }
-})
+}
